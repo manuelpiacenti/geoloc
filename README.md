029414026ca6dc08e22427bf0822c1b6f465ff59
manuelpiacenti <35454305+manuelpiacenti@users.noreply.github.com>
2024-06-22 18:26:35 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# geoloc
+# Getgeoloc
+
+## Getting started
+
+Use this homely bash script to get MaxMind geolocation from RIPE.
+
+## Installaed package requirements
+
+- whois
+- jq
+
+```apt install whois jq```
---
+++ getgeoloc.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "Hello, please enter ASN..."
+
+echo -n "ASN to get geoloc of ASN prefix: "
+read -r as
+
+re='^[0-9]+$'
+if ! [[ $as =~ $re ]] ; then
+   echo "error: Not a valid ASN, please retry" >&2; exit 1
+fi
+
+case $as in 
+    *[![:digit:]]*) onlydigits=0;; # contains non-digits
+    *[[:digit:]]*)  onlydigits=1;; # at least one digit
+    *)              onlydigits=0;; # empty
+esac
+
+if [ $onlydigits = 0 ]; then
+    echo "'$as' is empty or contains something other than digits" >&2; exit 1
+elif [ "${#as}" -le 2 ]; then
+    echo "'$as' contains 1 to 3 digits (and nothing else)" >&2; exit 1
+fi
+
+whois -h whois.ripe.net -- "-i origin AS$as" | awk '/^route:/ {print $2;}' > /tmp/list_prefix_as.txt
+NUMB_LINE=1
+filetoread=/tmp/list_prefix_as.txt
+
+echo prefix \| country \| city
+while read line;
+do 
+    prefix=$(cat $filetoread | awk 'NR=='$NUMB_LINE'{print $1}')
+    country=$(curl -s --location --request GET "https://stat.ripe.net/data/maxmind-geo-lite/data.json?resource=$prefix" | jq -r '.data.located_resources[]' | grep country | awk '{print $2}' | awk -F'"' '$0=$2')
+    city=$(curl -s --location --request GET "https://stat.ripe.net/data/maxmind-geo-lite/data.json?resource=$prefix" | jq -r '.data.located_resources[]' | grep city | awk '{print $2}' | awk -F'"' '$0=$2')
+    echo $prefix \| $country \| $city    
+    NUMB_LINE=$((NUMB_LINE+1))
+done < $filetoread
+rm -rf /tmp/list_prefix_as.txt
+exit 0
+
